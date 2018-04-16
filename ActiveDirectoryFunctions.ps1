d0ac30aa9133718df7e148ba6e7eaf298ec1a3a8
Mike Campbell <mike@exactsolutions.biz>
2018-04-16 12:20:32 -0400

    added to ActiveDirectoryFunctions.ps1 and added TestFunctions.ps1

--- ActiveDirectoryFunctions.ps1
+++ ActiveDirectoryFunctions.ps1
@@ -710,3 +710,54 @@ Function Get-ADDomainNetBiosName
     #Return the NetBIOSName
     Write-Output $NetBiosName
 }#Get-ADDomainNetBiosName
+function Test-ADPsDrive
+{
+    [cmdletbinding()]
+    param
+    (
+        [string]$Name
+        ,
+        [switch]$IsRootofDirectory
+    )
+
+    #Check PSDrive:  Should be AD, Should be Root of the PSDrive
+    Try
+    {
+        $ADPSDrive = Get-PSDrive -name $name -PSProvider ActiveDirectory -ErrorAction Stop
+    }
+    Catch
+    {
+        Write-Verbose -message "No PSDrive with Name $name and PSProviderType ActiveDirectory exists."
+        $false
+    }
+
+    $PSDriveTests = @{
+        ProviderIsActiveDirectory = $($ADPSDrive.Provider.name -eq 'ActiveDirectory')
+    }
+
+    if ($IsRootDSE)
+    {
+        $psdriveTests.RootIsRootOfDirectory = ($ADPSDrive.Root -eq '//RootDSE/')
+    }
+
+    if ($PSDriveTests.Values -contains $false)
+    {
+        $false
+    }
+    else
+    {
+        $true
+    }
+}
+#end function Test-ADPSDrive
+function Test-StringIsConvertibleToGUID
+{
+    [CmdletBinding()]
+    param
+    (
+        [parameter(Mandatory, ValueFromPipeline)]
+        [String]$string
+    )
+    try {([guid]$string -is [guid])} catch {$false}
+}
+#end function TestStringIsConvertibleToGUID
