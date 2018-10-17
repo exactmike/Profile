000e79a0c84e71417779763cd3acc2024093c783
Campbell <micampbell@changehealthcare.com>
2018-10-17 13:55:58 -0400

    add function Get-uncpath

--- UtilityFunctions.ps1
+++ UtilityFunctions.ps1
@@ -568,7 +568,6 @@ function Export-Credential
     $exportCredential
 }
 #end function Export-Credential
-
 function Show-One
 {
     [cmdletbinding()]
@@ -589,3 +588,57 @@ function Show-One
         }
     }
 }
+function Get-UNCPath {
+    [cmdletbinding()]
+    param (
+        [parameter(ValueFromPipeline,ValueFromPipelineByPropertyName)]
+        [validatescript({Test-Path -Path $_})]
+        [string[]]$Path = $(Get-Location).Path
+    )
+    begin
+    {
+        function Get-ContainerUNCPath
+        {
+            param(
+                $ContainerPath
+            )
+            Push-Location
+            Set-Location -Path $ContainerPath
+            $loc = Get-Location
+            if ($null -eq $loc.Drive) {$loc.ProviderPath} 
+            else {
+                switch ($null -eq $loc.Drive.DisplayRoot)
+                {
+                    $true #not a network mapped drive - is local.  Might not work for drives mapped to a local subdirectory? need to test and revise if not
+                    {
+                        Join-Path -path (Join-Path -Path $('\\' + [System.Environment]::MachineName) -ChildPath $($loc.Drive.Name + '$')) -ChildPath $loc.Drive.CurrentLocation
+                    }
+                    $false #is a network mapped drive
+                    {
+                        Join-Path -Path $loc.Drive.DisplayRoot -ChildPath $loc.Drive.CurrentLocation   
+                    }
+                }
+            }
+            Pop-Location
+        }
+    }
+    process
+    {
+        foreach ($p in $Path)
+        {
+            $item = Get-Item -Path $p
+            switch ($item.PSIsContainer)
+            {
+                $true
+                {
+                    Get-ContainerUNCPath -ContainerPath $item.FullName
+                }
+                $false
+                {
+                    Join-Path -Path $(Get-ContainerUNCPath -ContainerPath $(Split-Path -Path $item.FullName -Parent)) -ChildPath $item.name
+                }
+            }
+        }
+    }    
+}
+
