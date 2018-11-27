549674cae3d8cea5665703f6ab77d6bb9317d3b3
Mike Campbell <mike@exactsolutions.biz>
2018-11-27 11:32:13 -0500

    begin new function

--- PackageAndProfileManagementFunctions.ps1
+++ PackageAndProfileManagementFunctions.ps1
@@ -247,3 +247,12 @@ function Get-AvailableModuleInstallationStatus
 
     }
 }
+function Update-DesiredModule
+{
+    [CmdletBinding()]
+    param(
+        [parameter(ValueFromPipeline,ValueFromPipelineByPropertyName,ParameterSetName = 'Named')]
+        [string[]]$Name
+    )
+    #how to handle this - use AvailableModuleInstallationStatus as input?
+}
