d288ed262c8f74d7e3157694430971175531fc12
Mike Campbell <mike@exactsolutions.biz>
2018-12-03 19:29:31 -0500

    added function get-alphanumeric

--- UtilityFunctions.ps1
+++ UtilityFunctions.ps1
@@ -631,3 +631,15 @@ function Get-PSModulePath
 {
     $env:PSModulePath.split(';')
 }
+function Get-AlphaNumeric
+{
+    $numbers = 1..26
+    $letters = Get-CustomRange -first a -second z -type char
+    foreach ($n in $numbers)
+    {
+        [PSCustomObject]@{
+            Letter = $letters[($n-1)]
+            Number = $n
+        }
+    }
+}
