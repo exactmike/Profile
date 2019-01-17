8a4150bc47c4f37b1dd82b3f7b455e99aa51d77f
Mike Campbell <mike@exactsolutions.biz>
2019-01-17 07:43:37 -0500

    improvements to Out-FunctionFile

--- functions/Out-FunctionFile.ps1
+++ functions/Out-FunctionFile.ps1
@@ -8,9 +8,11 @@ function Out-FunctionFile
 
     $FileName = $Function.Name + '.ps1'
     $Contents = @"
-    Function $($Function.Name) {
+    Function $($Function.Name)
+    {
         $($Function.Scriptblock)
     }
+
 "@
     $outpath = Join-Path -Path $Path -ChildPath $FileName
     $Contents | Out-File -FilePath $outpath -Encoding utf8
