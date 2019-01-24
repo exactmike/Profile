e01384c5194028eabc140ce8b93f458eedb750fe
exactmike <mike@exactsolutions.biz>
2019-01-24 10:39:54 -0500

    fix keep alive so it doesn't start on module load . . . duh

--- functions/Start-KeepAlive.ps1
+++ functions/Start-KeepAlive.ps1
@@ -1,2 +1,4 @@
-$wshell = New-Object -ComObject wscript.shell
-do {$wshell.SendKeys('{SCROLLLOCK}'); Write-Host 'Sleeping 60 Seconds'; Start-Sleep -Seconds 60} until ($false)
+function Start-KeepAlive {
+    $wshell = New-Object -ComObject wscript.shell
+    do {$wshell.SendKeys('{SCROLLLOCK}'); Write-Host 'Sleeping 60 Seconds'; Start-Sleep -Seconds 60} until ($false)
+}
