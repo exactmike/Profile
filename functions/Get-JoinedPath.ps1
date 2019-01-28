589e3c2f9433480122c312725528179bdf7c418a
Mike Campbell <mike@exactsolutions.biz>
2019-01-28 12:47:00 -0500

    something new

---
+++ functions/Get-JoinedPath.ps1
@@ -0,0 +1,12 @@
+function Get-JoinedPath
+{
+    [cmdletbinding()]
+    param(
+        [string[]]$Path
+    )
+    foreach ($p in $Path)
+    {
+        if ($p -contains ':' -and $p -notmatch '')
+    }
+    [IO.Path]::Combine($path)
+}
