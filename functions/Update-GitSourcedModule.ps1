e56753fc4b3d84d1c456b2e342a6337a4c57866c
exactmike <mike@exactsolutions.biz>
2019-01-29 13:37:09 -0500

    updates to Update-GitSourcedModule

--- functions/Update-GitSourcedModule.ps1
+++ functions/Update-GitSourcedModule.ps1
@@ -1,8 +1,8 @@
 ﻿    Function Update-GitSourcedModule {
-        
+
     [cmdletbinding()]
     param(
-        $path = $(pwd).path
+        $path = $(Get-Location).path
         ,
         [switch]$Recurse
     )
@@ -20,35 +20,43 @@
         foreach ($gsm in $GitSourcedModules)
         {
             Set-Location -LiteralPath $gsm
-            $GitStatus = Get-GitStatus
             $Name = Split-Path -Leaf -Path $gsm
+            $GitStatus = Get-GitStatus | Select-Object -property @{n='Name';e={$Name}},Branch,Upstream,BehindBy,AheadBy,HasWorking
+            Write-Information -MessageData $GitStatus
             $Message = "Fetching $Name from $($GitStatus.Upstream)"
-            Write-Verbose -Message $Message
+            Write-Information -MessageData $Message
             git fetch
             $GitStatus = Get-GitStatus
             switch ($GitStatus.HasWorking)
             {
                 $true
                 {
-                    $Message = "$($cd.PSChildName) has $($gitStatus.Working.Count) file(s) with local uncommitted changes: $($GitStatus.Working -join ', ')"
-                    Write-Warning -Message $Message
+                    $Message = "$($Name) has $($gitStatus.Working.Count) file(s) with local uncommitted changes: $($GitStatus.Working -join ', ')"
+                    Write-Information -MessageData $Message -Tags Warning
                 }
                 $false
                 {
                     if ($GitStatus.BehindBy -gt 0)
                     {
-                        $Message = "Pull $($cd.PSChildName) for $($GitStatus.Branch) from $($GitStatus.Upstream)"
-                        Write-Verbose -Message $Message
+                        $Message = "Pull $($Name) for $($GitStatus.Branch) from $($GitStatus.Upstream)"
+                        Write-Information -MessageData $Message
                         git pull
                     }
                     if ($GitStatus.AheadBy -gt 0)
                     {
-                        $Message = "Push $($cd.PSChildName) for $($GitStatus.Branch) to $($GitStatus.Upstream)"
-                        Write-Verbose -Message $Message
+                        $Message = "Push $($Name) for $($GitStatus.Branch) to $($GitStatus.Upstream)"
+                        Write-Information -MessageData $Message
                         git push
+                    }
+                    if ($GitStatus.BehindBy -eq 0 -and $gitStatus.AheadBy -eq 0)
+                    {
+                        $Message = "$Name is even with $($GitStatus.Upstream)"
+                        Write-Information -MessageData $Message
                     }
                 }
             }
+            $GitStatus = Get-GitStatus | Select-Object -property @{n='Name';e={$Name}},Branch,Upstream,BehindBy,AheadBy,HasWorking
+            Write-Information -MessageData $GitStatus
         }
         Pop-Location
     }
