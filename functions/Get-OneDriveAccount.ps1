ff17b3b29940d3cefd7c585baa3d58d13cfbe4b9
MIke Campbell <mike@exactsolutions.biz>
2019-11-01 12:07:29 -0400

    replace get-onedrivefolder with get-onedrive account

---
+++ functions/Get-OneDriveAccount.ps1
@@ -0,0 +1,52 @@
+﻿Function Get-OneDriveAccount
+{
+    [CmdletBinding()]
+    param(
+        [parameter()]
+        [AllowNull()]
+        [AllowEmptyString()]
+        [string[]]$DisplayName
+        ,
+        [parameter()]
+        [AllowNull()]
+        [AllowEmptyString()]
+        [string[]]$UserEmail
+        ,
+        [parameter()]
+        [AllowNull()]
+        [guid[]]$TenantID
+    )
+    if (Test-Path HKCU:\Software\Microsoft\OneDrive\Accounts -PathType Container)
+    {
+        @(Get-ChildItem -Path HKCU:\Software\Microsoft\OneDrive\Accounts).foreach(
+            { Get-ItemProperty "Registry::$($_.name)" }
+        ).where(
+            {
+                (
+                    $null -eq $DisplayName -or [string]::IsNullOrEmpty($DisplayName) -or
+                    $_.DisplayName -in $DisplayName -or
+                    $(
+                        @(foreach ($dn in $DisplayName)
+                            {
+                                $_.DisplayName -like $dn
+                            }) -contains $true)
+                ) -and
+                (
+                    $null -eq $userEmail -or [string]::IsNullOrEmpty($UserEmail) -or
+                    $_.UserEmail -in $UserEmail -or
+                    $(
+                        @(foreach ($ue in $UserEmail)
+                            {
+                                $_.UserEmail -like $ue
+                            }) -contains $true)
+                ) -and
+                (
+                    $null -eq $TenantID -or
+                    $_.ConfiguredTenantId -in @($TenantID.foreach( { $_.guid }))
+                )
+            }
+        )
+
+    }
+
+}
--- functions/Get-OneDriveFolder.ps1
+++
@@ -1,9 +0,0 @@
-﻿    Function Get-OneDriveFolder {
-        
-    if (Test-Path HKCU:\Software\Microsoft\OneDrive)
-    {$OneDrivePath = (Get-ItemProperty -Path HKCU:\Software\Microsoft\OneDrive -Name UserFolder).UserFolder}
-    elseif (Test-Path HKCU:\Software\Microsoft\Windows\CurrentVersion\SkyDrive)
-    {$OneDrivePath = (Get-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\SkyDrive -Name UserFolder).UserFolder}
-    $OneDrivePath
-
-    }
