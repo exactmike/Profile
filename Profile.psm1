9928354ec2640f90e18fb3cfd87cc7c9bc8dc666
MIke Campbell <mike@exactsolutions.biz>
2019-11-01 12:05:51 -0400

    fix profile.psm1 to only run powershell files

--- Profile.psm1
+++ Profile.psm1
@@ -1,4 +1,4 @@
 ﻿$FunctionFiles = Get-ChildItem -Recurse -File -Path $(Join-Path -Path $PSScriptRoot -ChildPath 'Functions')
-foreach ($ff in $FunctionFiles) { . $ff.fullname }
+foreach ($ff in $FunctionFiles) { if ($ff.fullname -like '*.ps*1') { . $ff.fullname } }
 
 enum InstallManager { Chocolatey; Git; PowerShellGet; Manual }
