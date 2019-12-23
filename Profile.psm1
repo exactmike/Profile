d93227899c9b611784f5d7c14dfb1f3998cff525
Mike Campbell <mike@exactsolutions.biz>
2019-12-22 20:49:15 -0500

    remove an enum that is now in InstallManager

--- Profile.psm1
+++ Profile.psm1
@@ -1,4 +1,2 @@
 ﻿$FunctionFiles = Get-ChildItem -Recurse -File -Path $(Join-Path -Path $PSScriptRoot -ChildPath 'Functions')
 foreach ($ff in $FunctionFiles) { if ($ff.fullname -like '*.ps*1') { . $ff.fullname } }
-
-enum InstallManager { Chocolatey; Git; PowerShellGet; Manual }
