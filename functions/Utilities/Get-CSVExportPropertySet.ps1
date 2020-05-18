47798ab5d17b97fb9c2aed613ee70e5c9de7fb01
Mike Campbell <mike@campbellnetwork.net>
2020-05-18 17:49:04 -0400

    update Get-CSVExportPropertySet.  Add Sort Parameter. Fix Attribute order output.

--- functions/Utilities/Get-CSVExportPropertySet.ps1
+++ functions/Utilities/Get-CSVExportPropertySet.ps1
@@ -1,5 +1,6 @@
-﻿    Function Get-CSVExportPropertySet {
-        
+﻿Function Get-CSVExportPropertySet
+{
+
     <#
             .SYNOPSIS
             Creates an array of property definitions to be used with Select-Object to prepare data with multi-valued attributes for export to a flat file such as csv.
@@ -11,11 +12,13 @@
             Used to specify the custom delimiter to be used between multi-valued entries in the multi-valued attributes input array.  Default is "|" if not specified.  Avoid using a "," if exporting data to a csv file later in your pipeline.
 
             .PARAMETER  MultiValuedAttributes
-            An array of attributes from your source data which you expect to contain multiple values.  These will be converted to @{n=[PropertyName];e={$_.$propertyname -join $Delimiter} in the output of the function.
+            An array of attributes from your source data which you expect to contain multiple values.  These will be converted to @{n=[PropertyName];e={$_.$propertyname -join $Delimiter} in the output of the function. The order of the properties in the output will be Scalar attributes (in the order provided), then MultiValuedAttributes (in the order provided).
 
             .PARAMETER  ScalarAttributes
-            An array of attributes from your source data which you expect to contain scalar values.  These will be passed through directly in the output of the function.
+            An array of attributes from your source data which you expect to contain scalar values.  These will be passed through directly in the output of the function. The order of the properties in the output will be Scalar attributes (in the order provided), then MultiValuedAttributes (in the order provided).
 
+            .PARAMETER  Sort
+            A switch parameter to indicate that you want to sort the MultiValuedAttributes with default sorting
 
             .EXAMPLE
             Get-CSVExportPropertySet -Delimiter ';' -MultiValuedAttributes proxyaddresses,memberof -ScalarAttributes userprincipalname,samaccountname,targetaddress,primarysmtpaddress
@@ -43,20 +46,37 @@
         [string[]]$ScalarAttributes
         ,
         [switch]$SuppressCommonADProperties
+        ,
+        [switch]$Sort
     )
     $ADUserPropertiesToSuppress = @('CanonicalName', 'DistinguishedName')
     $CSVExportPropertySet = @()
     foreach ($mv in $MultiValuedAttributes)
     {
-        $ExpressionString = "`$_." + $mv + " -join '$Delimiter'"
+        $ExpressionString = "`$(`$_." + $mv
+
+        if ($true -eq $Sort)
+        {
+            $ExpressionString = $ExpressionString + ' | Sort-Object)'
+        }
+        else
+        {
+            ')'
+        }
+
+        $ExpressionString = $ExpressionString + " -join '$Delimiter'"
+
         $CSVExportPropertySet +=
         @{
             n = $mv
             e = [scriptblock]::Create($ExpressionString)
         }
     }#foreach
-    if ($SuppressCommonADProperties) {$CSVExportPropertySet += ($ScalarAttributes | Where-Object {$ADUserPropertiesToSuppress -notcontains $_})}
-    else {$CSVExportPropertySet += $ScalarAttributes}
-    $CSVExportPropertySet
 
-    }
+    $outputPropertySet =
+    if ($SuppressCommonADProperties) { ($ScalarAttributes | Where-Object { $ADUserPropertiesToSuppress -notcontains $_ }) + $CSVExportPropertySet }
+    else { $ScalarAttributes + $CSVExportPropertySet }
+
+    $outputPropertySet
+
+}
