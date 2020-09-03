75448e290d31d5af99136340d1507bf5b9a9274f
exactmike <mike@campbellnetwork.net>
2020-09-03 09:52:59 -0400

    keep alive change

--- functions/Utilities/Start-KeepAlive.ps1
+++ functions/Utilities/Start-KeepAlive.ps1
@@ -1,4 +1,11 @@
-function Start-KeepAlive {
+function Start-KeepAlive
+{
     $wshell = New-Object -ComObject wscript.shell
-    do {$wshell.SendKeys('{SCROLLLOCK}'); Write-Host 'Sleeping 60 Seconds'; Start-Sleep -Seconds 60} until ($false)
+    do
+    {
+        $wshell.SendKeys('{F13}')
+        Write-Host 'Sleeping 60 Seconds'
+        Start-Sleep -Seconds 60
+    }
+    until ($false)
 }
