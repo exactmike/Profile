357f66a6d2e4ca450aeaff1fee9ae59780f95d49
exactmike <mike@campbellnetwork.net>
2020-12-03 11:02:03 -0500

    20201203

--- functions/TestFunctions/Test-CurrentPrincipalIsAdmin.ps1
+++ functions/TestFunctions/Test-CurrentPrincipalIsAdmin.ps1
@@ -1,6 +1,7 @@
-﻿    Function Test-CurrentPrincipalIsAdmin {
-        
+﻿Function Test-CurrentPrincipalIsAdmin
+{
+
     $currentPrincipal = ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent())
     $currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole] 'Administrator')
 
-    }
+}
