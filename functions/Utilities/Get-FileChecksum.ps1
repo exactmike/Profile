00128a7f20215ffe13cab3ba9d89cb7d5a45298a
ThatExactMike <mike@campbellnetwork.net>
2021-02-08 13:17:16 -0500

    improve & rename get-checksum to get-filechecksum

---
+++ functions/Utilities/Get-FileChecksum.ps1
@@ -0,0 +1,20 @@
+﻿Function Get-FileChecksum
+{
+
+    Param (
+        [parameter(Mandatory = $True)]
+        [ValidateScript( { Test-Path -path $_ -PathType Leaf })]
+        [string]$Path
+        ,
+        [ValidateSet('sha1', 'md5', 'sha512')]
+        [string]$Algorithm
+    )
+    $FileObject = Get-Item -Path $Path
+    $fs = [System.IO.FileStream]::new($($FileObject.FullName), 'Open' )
+    $algo = [type]"System.Security.Cryptography.$Algorithm"
+    $crypto = $algo::Create()
+    $hash = [BitConverter]::ToString($crypto.ComputeHash($fs)).Replace('-', '')
+    $fs.Close()
+    $hash
+
+}
