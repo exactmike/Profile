6275a5c563f05305f2b4d2f6343d092e27b7d5f7
Mike Campbell <exactmike@users.noreply.github.com>
2023-04-10 19:28:33 -0400

    add the Group-Join function

---
+++ functions/Utilities/Group-Join.ps1
@@ -0,0 +1,55 @@
+Function Group-Join
+{
+    [CmdletBinding()]
+    param(
+        [Parameter(ValueFromPipeline)]
+        [psobject]
+        ${InputObject},
+
+        [Parameter(ParameterSetName='DefaultParameter', Position=0)]
+        [System.Object[]]
+        #Property by which to group objects
+        ${Property},
+
+        [Parameter(ParameterSetName='DefaultParameter', Mandatory)]
+        [string[]]
+        ${JoinProperty},
+
+        [Parameter(ParameterSetName='DefaultParameter', Mandatory)]
+        [string]
+        ${JoinDelimeter}
+    )
+    begin
+    {
+        $collection = [System.Collections.Generic.List[psobject]]::new()
+    }
+
+    process
+    {
+        $collection.Add($InputObject)
+    }
+
+    end
+    {
+        $GroupedCollection = @($collection | Group-Object -Property $Property)
+        $GroupedCollection.foreach({
+                $group = $_
+                switch ($group.count)
+                {
+                    1
+                    {$group.group}
+                    {$_ -ge 2}
+                    {
+                        $JPHash = @{}
+                        $JoinProperty.foreach({
+                                $JPHash.$_ = $group.group.$_ -join $JoinDelimeter
+                            })
+                        $JPHash.Keys.foreach({
+                                $group.group[0].$_ = $JPHash.$_
+                            })
+                        $group.group[0]
+                    }
+                }
+            })
+    }
+}
