2f42ffa0028ca455208abeae997523ac6cc59a08
Mike Campbell <exactmike@users.noreply.github.com>
2024-09-21 12:12:52 -0400

    add help to Compare-ComplexObject

--- functions/Utilities/Compare-ComplexObject.ps1
+++ functions/Utilities/Compare-ComplexObject.ps1
@@ -1,21 +1,118 @@
-﻿Function Compare-ComplexObject
+﻿<#
+.SYNOPSIS
+    Compares one object to another object at the attribute level.
+    Does what you might expect Compare-Object to do but Compare-Object only compares SETS of objects and is most effective at comparing simple arrays.
+    Compare-ComplexObject actually compares two objects at the attribute level to give a report of similarities and differences.
+.DESCRIPTION
+    Compares one object to anotehr object and outputs an object summarizing the similarities and / or differences of each attribute.
+    The first object is the reference object and the second object is the difference object.
+    Compare-ComplexObject is useful for generating a differences object between a before and after of a configuration of an object or for updating only changed attributes between objects stored in differents systems.
+.NOTES
+    This function, by default, overrides the default Powershell comparison of boolean $false to string 'false'.  The function returns TRUE, meaning equality, in this case.
+    The reason for the override is that the default Powershell comparison of string 'false' to boolean 'false' returns TRUE, so for the purposes of this function the revers should return true as well.
+    When using switch parameter DisableBoolNonBoolEquality, all of Powershell's default comparisons between boolean $true or $false and other object types are overridden.
+    When using DisableBoolNonBoolEquality, the compared values must both be of type Boolean and the same value to get a TRUE comparison result.
+
+.EXAMPLE
+    # Define the first user
+    $user1 = [PSCustomObject]@{
+        FirstName  = 'Arvind'
+        LastName   = 'Sharma'
+        Location   = 'New York'
+        LocationCountry = 'US'
+        EmployeeID = 'A12345'
+        Email      = 'arvind.sharma@example.com'
+        EmployeeType = 'FullTime'
+        Codes = @('alpha','beta','gamma')
+        Active = $true
+        OnLeave = $false
+    }
+
+    # Define the second user
+    $user2 = [PSCustomObject]@{
+        FirstName  = 'Carla'
+        LastName   = 'Martinez'
+        Location   = 'Los Angeles'
+        LocationCountry = 'US'
+        EmployeeID = 'C67890'
+        Email      = 'carla.martinez@example.com'
+        EmployeeType = 'FullTime'
+        Codes = @('beta','gamma')
+        Active = 'true'
+        OnLeave = 'false'
+    }
+
+    Compare-ComplexObject -ReferenceObject $user1 -DifferenceObject $user2 # Default shows ALL comparisons
+
+    Property        CompareResult ReferenceObjectValue        DifferenceObjectValue
+    --------        ------------- --------------------        ---------------------
+    Active                   True {True}                      {true}
+    Codes                   False {alpha, beta, gamma}        {beta, gamma}
+    Email                   False {arvind.sharma@example.com} {carla.martinez@example.com}
+    EmployeeID              False {A12345}                    {C67890}
+    EmployeeType             True {FullTime}                  {FullTime}
+    FirstName               False {Arvind}                    {Carla}
+    LastName                False {Sharma}                    {Martinez}
+    Location                False {New York}                  {Los Angeles}
+    LocationCountry          True {US}                        {US}
+    OnLeave                  True {False}                     {false}
+
+    Compare-ComplexObject -ReferenceObject $user1 -DifferenceObject $user2 -Show DifferentOnly
+
+    Property   CompareResult ReferenceObjectValue        DifferenceObjectValue
+    --------   ------------- --------------------        ---------------------
+    Codes              False {alpha, beta, gamma}        {beta, gamma}
+    Email              False {arvind.sharma@example.com} {carla.martinez@example.com}
+    EmployeeID         False {A12345}                    {C67890}
+    FirstName          False {Arvind}                    {Carla}
+    LastName           False {Sharma}                    {Martinez}
+    Location           False {New York}                  {Los Angeles}
+
+    Compare-ComplexObject -ReferenceObject $user1 -DifferenceObject $user2 -Show EqualOnly
+
+    Property        CompareResult ReferenceObjectValue DifferenceObjectValue
+    --------        ------------- -------------------- ---------------------
+    Active                   True {True}               {true}
+    EmployeeType             True {FullTime}           {FullTime}
+    LocationCountry          True {US}                 {US}
+    OnLeave                  True {False}              {false}
+
+    Compare-ComplexObject -ReferenceObject $user1 -DifferenceObject $user2 -show DifferentOnly -DisableBoolNonBoolEquality
+
+    Property   CompareResult ReferenceObjectValue        DifferenceObjectValue
+    --------   ------------- --------------------        ---------------------
+    Active             False {True}                      {true}
+    Codes              False {alpha, beta, gamma}        {beta, gamma}
+    Email              False {arvind.sharma@example.com} {carla.martinez@example.com}
+    EmployeeID         False {A12345}                    {C67890}
+    FirstName          False {Arvind}                    {Carla}
+    LastName           False {Sharma}                    {Martinez}
+    Location           False {New York}                  {Los Angeles}
+    OnLeave            False {False}                     {false}
+#>
+
+
+Function Compare-ComplexObject
 {
     [cmdletbinding()]
     param(
         [Parameter(Mandatory)]
-        $ReferenceObject
+        $ReferenceObject # The Reference Object for the comparison
         ,
         [Parameter(Mandatory)]
-        $DifferenceObject
-        ,
-        [string[]]$SuppressedProperties
+        $DifferenceObject # The Difference Object for the comparison
         ,
         [parameter()]
+        [Alias('ExcludeProperty')]
+        [string[]]$SuppressedProperties # Properties to leave out of the comparison
+        ,
+        [parameter()]
+        [Alias('Include')]
         [validateset('All', 'EqualOnly', 'DifferentOnly')]
-        [string]$Show = 'All'
+        [string]$Show = 'All' # Specifies which comparison results to include in the output
         ,
         [parameter()]
-        [switch]$DisableBoolNonBoolEquality
+        [switch]$DisableBoolNonBoolEquality # Forces Boolean to Boolean comparison, overriding Powershell default Boolean to other type behavior. If used, the compared properties must both be Boolean for a TRUE response to be possible.
     )
 
     #setup properties to compare
