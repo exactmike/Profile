b3bf479a3cfed55617a82edb302fb2aed102c0ef
Mike Campbell <exactmike@users.noreply.github.com>
2024-11-30 13:14:25 -0500

    fix case mismatch in psm1

--- Profile.psm1
+++ Profile.psm1
@@ -1,2 +1,5 @@
-﻿$FunctionFiles = Get-ChildItem -Recurse -File -Path $(Join-Path -Path $PSScriptRoot -ChildPath 'Functions')
-foreach ($ff in $FunctionFiles) { if ($ff.fullname -like '*.ps*1') { . $ff.fullname } }
+﻿$FunctionsFolder = Join-Path -Path $PSScriptRoot -ChildPath 'functions'
+Write-Verbose -Message "Finding Functions from $FunctionsFolder"
+$FunctionFiles = Get-ChildItem -Recurse -File -Path $FunctionsFolder
+Write-Verbose -Message "Found $($FunctionFiles.count) Function Files To Load"
+foreach ($ff in $FunctionFiles) { if ($ff.fullname -like '*.ps1') { . $ff.fullname } }
